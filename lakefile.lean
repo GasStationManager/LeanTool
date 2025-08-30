539e4c2403be1b822c8f1ed27dacd5dea8be96a6
GasStationManager <gasstationcodemanager@gmail.com>
2025-08-30 03:45:01 +0000

    bump to 4.22.0

--- lakefile.lean
+++ lakefile.lean
@@ -8,10 +8,10 @@ package "LeanTool" where
   ]
   -- add any additional package configuration options here
 
-require Hammer from git "https://github.com/JOSHCLUNE/LeanHammer" @ "v4.21.0"
+require Hammer from git "https://github.com/JOSHCLUNE/LeanHammer" @ "v4.22.0"
 
 
-require "leanprover-community" / "mathlib" @ git "v4.21.0"
+require "leanprover-community" / "mathlib" @ git "v4.22.0"
 
 
 @[default_target]
